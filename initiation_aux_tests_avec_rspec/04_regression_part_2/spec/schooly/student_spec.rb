ce19e1f8ca69163251197d92bd61312cb79e9e9e
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2018-06-26 12:09:00 +0200

    test: clean

--- initiation_aux_tests_avec_rspec/04_regression_part_2/spec/schooly/student_spec.rb
+++ initiation_aux_tests_avec_rspec/04_regression_part_2/spec/schooly/student_spec.rb
@@ -1,13 +1,9 @@
 RSpec.describe Schooly::Student do
-  [{
-    firstname: 'John',
-    lastname: 'Doe',
-    id: 'ae6e4d1209f17b460503904fad297b31e9cf6362'
-  }].each do |student_hash|
-    it "return #{student_hash[:id]} id for #{student_hash[:firstname]} #{student_hash[:lastname]}" do
-      john_doe = described_class.new(firstname: student_hash[:firstname], lastname: student_hash[:lastname])
+  before(:all) do
+    @john_doe = described_class.new(firstname: 'John', lastname: 'Doe')
+  end
 
-      expect(john_doe.id).to eql(student_hash[:id])
-    end
+  it "return expected id for John Doe" do
+    expect(@john_doe.id).to eql('ae6e4d1209f17b460503904fad297b31e9cf6362')
   end
 end
